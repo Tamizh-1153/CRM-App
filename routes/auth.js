b9c52eae29ed2104982cc489cef4eba4f82bb143
Tamizh-1153 <128562854+Tamizh-1153@users.noreply.github.com>
2023-06-20 14:09:19 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express=require('express')
 const router=express.Router()
 
 
-const {login,register,forgotPassword}=require('../controllers/auth')
+const {login,register,forgotPassword,resetPassword}=require('../controllers/auth')
 
 router.post('/register',register)
 router.post('/login',login)
 router.post('/forgotPassword',forgotPassword)
+router.post('/resetPassword/:id/:token',resetPassword)
 
 module.exports=router
