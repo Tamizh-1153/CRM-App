277d8037f2634d07c689d90247eb39828d5f6f74
Tamizh-1153 <128562854+Tamizh-1153@users.noreply.github.com>
2023-06-20 14:07:44 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,8 +83,28 @@ const forgotPassword = async (req, res) => {
   }
 }
 
+const resetPassword = async (req, res) => {
+  const { id, token } = req.params
+  const {password}=req.body
+  const user = await User.findOne({ _id: id })
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" })
+  }
+  if(user.token!=token) {
+    return res.status(404).json({ message: "Invalid token" })
+  }
+
+  user.password=password
+  user.token=''
+  await user.save()
+  res.json({status:'password reset success'})
+
+}
+
 module.exports = {
   register,
   login,
-  forgotPassword
+  forgotPassword,
+  resetPassword
 }
